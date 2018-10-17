49c65f8722341baad36ab635be19fca678f6c94c
ForYaSee <foryasee@gmail.com>
2018-10-17 15:44:30 +0200

    Initial Code

---
+++ src/main/java/asia/devs/from/discordjar/Discord.java
@@ -0,0 +1,4 @@
+package asia.devs.from.discordjar;
+
+public class Discord {
+}
---
+++ src/test/java/TestDiscord.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class TestDiscord {
+
+    @Test
+    public void testDiscord() {
+
+    }
+}
